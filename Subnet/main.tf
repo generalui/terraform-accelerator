f723e4ac30303c80cc9046ad9a11695c15e1ecf4
Jon Ryser <241263+jonryser@users.noreply.github.com>
2023-09-21 00:27:11 -0700

    Fixed Subnet name typo

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -3,8 +3,8 @@ module "dynamic_subnets" {
   version = "2.4.1"
 
   name      = var.name == null ? var.context.name : var.name
-  namespace = var.name == null ? var.context.name : var.name
-  stage     = var.name == null ? var.context.name : var.name
+  namespace = var.namespace == null ? var.context.namespace : var.namespace
+  stage     = var.stage == null ? var.context.stage : var.stage
 
   availability_zones = var.availability_zones
   ipv4_cidr_block    = var.ipv4_cidr_block
