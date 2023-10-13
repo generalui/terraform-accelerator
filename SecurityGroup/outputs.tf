99b276ceed0a644e8a3b7b44f840e70d56cc9701
Jon Ryser <241263+jonryser@users.noreply.github.com>
2023-10-13 15:17:49 -0700

    Use accepted naming convention for modules.

--- SecurityGroup/outputs.tf
+++ SecurityGroup/outputs.tf
@@ -1,19 +1,19 @@
 output "arn" {
   description = "The created Security Group ARN (null if using existing security group)"
-  value       = module.security-group.arn
+  value       = module.security_group.arn
 }
 
 output "id" {
   description = "The created or target Security Group ID"
-  value       = module.security-group.id
+  value       = module.security_group.id
 }
 
 output "name" {
   description = "The created Security Group Name (null if using existing security group)"
-  value       = module.security-group.name
+  value       = module.security_group.name
 }
 
 output "rules_terraform_ids" {
   description = "List of Terraform IDs of created `security_group_rule` resources, primarily provided to enable `depends_on`"
-  value       = module.security-group.rules_terraform_ids
+  value       = module.security_group.rules_terraform_ids
 }
