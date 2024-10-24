2f4594d175762ff0aa79eeee5ef901cf64c657b9
Jon Ryser <241263+jonryser@users.noreply.github.com>
2024-10-24 09:30:22 -0700

    [DocumentDB] Added Context

--- DocumentDB/main.tf
+++ DocumentDB/main.tf
@@ -2,9 +2,7 @@ module "documentdb_cluster" {
   source  = "cloudposse/documentdb-cluster/aws"
   version = "0.22.0"
 
-  name      = var.name == null ? var.context.name : var.name
-  namespace = var.namespace == null ? var.context.namespace : var.namespace
-  stage     = var.stage == null ? var.context.stage : var.stage
+  context = module.this.context
 
   allowed_security_groups = var.allowed_security_groups
   cluster_size            = var.cluster_size
@@ -14,6 +12,4 @@ module "documentdb_cluster" {
   subnet_ids              = var.subnet_ids
   vpc_id                  = var.vpc_id
   zone_id                 = var.zone_id
-
-  tags = var.tags == null ? var.context.tags : var.tags
 }
--- DocumentDB/variables.tf
+++ DocumentDB/variables.tf
@@ -10,24 +10,6 @@ variable "cluster_size" {
   description = "Number of DB instances to create in the cluster"
 }
 
-variable "context" {
-  type = any
-  default = {
-    attributes = []
-    name       = null
-    namespace  = null
-    stage      = null
-    tags       = {}
-  }
-  description = <<-EOT
-    Single object for setting entire context at once.
-    See description of individual variables for details.
-    Leave string and numeric variables as `null` to use default value.
-    Individual variable settings (non-null) override settings in context object,
-    except for attributes, tags, and additional_tag_map, which are merged.
-  EOT
-}
-
 # https://docs.aws.amazon.com/documentdb/latest/developerguide/limits.html#suported-instance-types
 variable "instance_class" {
   type        = string
@@ -47,40 +29,9 @@ variable "master_username" {
   description = "(Required unless a snapshot_identifier is provided) Username for the master DB user"
 }
 
-variable "name" {
-  type        = string
-  default     = null
-  description = <<-EOT
-    ID element. Usually the component or solution name, e.g. 'app' or 'jenkins'.
-    This is the only ID element not also included as a `tag`.
-    The "name" tag is set to the full `id` string. There is no tag with the value of the `name` input.
-    EOT
-}
-
-variable "namespace" {
-  type        = string
-  default     = null
-  description = "ID element. Usually an abbreviation of your organization name, e.g. 'eg' or 'cp', to help ensure generated IDs are globally unique"
-}
-
-variable "stage" {
-  type        = string
-  default     = null
-  description = "ID element. Usually used to indicate role, e.g. 'prod', 'staging', 'source', 'build', 'test', 'deploy', 'release'"
-}
-
 variable "subnet_ids" {
   type        = list(string)
   description = "List of VPC subnet IDs to place DocumentDB instances in"
-}
-
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = <<-EOT
-    Additional tags (e.g. `{'BusinessUnit': 'XYZ'}`).
-    Neither the tag keys nor the tag values will be modified by this module.
-    EOT
 }
 
 variable "vpc_id" {
